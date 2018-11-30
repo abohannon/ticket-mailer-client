b06be9da1e55e9329314f4b85fcafa6d8ffa14b5
Adam Bohannon <abo46n2@gmail.com>
2018-11-30 11:35:19 -0800

    Updated start script for production

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'build')))
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
-app.set('port', process.env.PORT || 8080)
+app.set('port', process.env.PORT || 3000)
 
 const server = app.listen(app.get('port'), () => {
   console.log('listening on port ', server.address().port)
