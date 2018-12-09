25607ffb667c6d5904c29ecab11a24e66e44f370
Adam Bohannon <abo46n2@gmail.com>
2018-12-08 20:04:02 -0800

    Debugging

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ import 'styles/antd-styles.css'
 import AppContainer from 'containers/AppContainer'
 
 const rootEl = document.getElementById('root')
+console.log('NODE_ENV', process.env.NODE_ENV)
+console.log(typeof window === 'object')
+console.log(typeof process === 'object')
 
 const renderApp = Component => render(
   <Root>
